ede5795aea657e82e6dc9227717721190e233ce8
Marco Garbelini <garbelini@gmail.com>
2023-09-24 21:32:31 +0200

    Removed custom profile parsing and only using the native AWS SDK instead

--- cmd/root.go
+++ cmd/root.go
@@ -5,13 +5,10 @@ import (
 	"fmt"
 	"os"
 
-	cfg "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/config"
 	awsS "github.com/aws/aws-sdk-go/aws"
 	"github.com/mattn/go-colorable"
 	"github.com/one2nc/cloudlens/internal"
-	"github.com/one2nc/cloudlens/internal/aws"
-	"github.com/one2nc/cloudlens/internal/color"
-	"github.com/one2nc/cloudlens/internal/config"
 	"github.com/one2nc/cloudlens/internal/view"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -45,7 +42,7 @@ func Execute() {
 	}
 }
 
-func run(cmd *cobra.Command, args []string) {
+func run(_ *cobra.Command, _ []string) {
 	mod := os.O_CREATE | os.O_APPEND | os.O_WRONLY
 	file, err := os.OpenFile("./cloudlens.log", mod, 0644)
 	if err != nil {
@@ -64,74 +61,20 @@ func run(cmd *cobra.Command, args []string) {
 	//var sess *session.Session
 	var regions []string
 	app := view.NewApp()
-	profiles, err := readAndValidateProfile()
-	if len(profiles) > 0 {
-		if profiles[0] == "default" && len(region) == 0 {
-			region = getDefaultAWSRegion()
-		} else if len(region) == 0 {
-			region = "ap-south-1"
-		}
 
-		regions = readAndValidateRegion()
-		//sess, err = aws.GetSession(profiles[0], regions[0])
-
-		cfg, err := aws.GetCfg(profiles[0], regions[0])
-		if err != nil {
-			panic(fmt.Sprintf("aws session init failed -- %v", err))
-		}
-
-		ctx := context.WithValue(context.Background(), internal.KeySession, cfg)
-		// TODO pass the AWS session instead of profiles and regions
-		if err := app.Init(ctx, profiles, regions, version); err != nil {
-			panic(fmt.Sprintf("app init failed -- %v", err))
-		}
-	} else {
-		profile := awsS.String(os.Getenv(AWS_PROFILE))
-		profiles := []string{*profile}
-		region := awsS.String(os.Getenv(AWS_DEFAULT_REGION))
-		regions := []string{*region}
-		cfg, err := aws.GetCfgUsingEnvVariables(profiles[0], regions[0])
-		if err != nil {
-			panic(fmt.Sprintf("aws session init failed -- %v", err))
-		}
-		ctx := context.WithValue(context.Background(), internal.KeySession, cfg)
-		if err := app.Init(ctx, profiles, regions, version); err != nil {
-			panic(fmt.Sprintf("app init failed -- %v", err))
-		}
+	profile := awsS.String(os.Getenv(AWS_PROFILE))
+	profiles := []string{*profile}
+	region := awsS.String(os.Getenv(AWS_DEFAULT_REGION))
+	regions = append(regions, *region)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic(fmt.Sprintf("aws session init failed -- %v", err))
+	}
+	ctx := context.WithValue(context.Background(), internal.KeySession, cfg)
+	if err := app.Init(ctx, profiles, regions, version); err != nil {
+		panic(fmt.Sprintf("app init failed -- %v", err))
 	}
 	if err := app.Run(); err != nil {
 		panic(fmt.Sprintf("app run failed %v", err))
 	}
-}
-
-func readAndValidateProfile() ([]string, error) {
-	profiles, err := aws.GetProfiles()
-	if err != nil {
-		fmt.Sprintf("failed to read profiles -- %v", err)
-		return nil, err
-	}
-	profiles, isSwapped := config.SwapFirstIndexWithValue(profiles, profile)
-	if !isSwapped {
-		fmt.Printf("Profile '%v' not found, using profile '%v'... ", color.Colorize(profile, color.Red), color.Colorize(profiles[0], color.Green))
-	}
-	return profiles, nil
-}
-
-func readAndValidateRegion() []string {
-	regions := aws.GetAllRegions()
-	regions, isSwapped := config.SwapFirstIndexWithValue(regions, region)
-	if !isSwapped {
-		fmt.Printf("Region '%v' not found, using %v..", color.Colorize(region, color.Red), color.Colorize(regions[0], color.Green))
-	}
-	return regions
-}
-
-func getDefaultAWSRegion() string {
-	cfg, err := cfg.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to load AWS SDK config: %v\n", err)
-		os.Exit(1)
-	}
-	region := cfg.Region
-	return region
 }
--- internal/aws/config.go
+++ internal/aws/config.go
@@ -4,18 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"log"
-	"os"
-	"strings"
-
 	awsV2 "github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
 	awsV2Config "github.com/aws/aws-sdk-go-v2/config"
-	creds "github.com/aws/aws-sdk-go-v2/credentials"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/defaults"
-	"gopkg.in/ini.v1"
+	"os"
 )
 
 type Profiles struct {
@@ -56,89 +48,4 @@ func GetCfg(profile, region string) (awsV2.Config, error) {
 		return awsV2.Config{}, errors.New("AWS Credentials expired")
 	}
 	return cfg, err
-}
-
-func GetCfgUsingEnvVariables(profile, region string) (awsV2.Config, error) {
-	akid := aws.String(os.Getenv(AWS_ACCESS_KEY_ID))
-	secKey := aws.String(os.Getenv(AWS_SECRET_ACCESS_KEY))
-	cfg, err := awsV2Config.LoadDefaultConfig(
-		context.TODO(),
-		awsV2Config.WithSharedConfigProfile(profile),
-		awsV2Config.WithRegion(region),
-		config.WithCredentialsProvider(
-			creds.NewStaticCredentialsProvider(*akid, *secKey, ""),
-		),
-	)
-	if err != nil {
-		fmt.Printf("failed to load config")
-		return awsV2.Config{}, err
-	}
-	creds, err := cfg.Credentials.Retrieve(context.TODO())
-	if err != nil {
-		fmt.Printf("failed to read credentials")
-		return awsV2.Config{}, err
-	}
-
-	credentialProvider := credentialProvider{Credentials: creds}
-	if credentialProvider.IsExpired() {
-		fmt.Println("Credentials have expired")
-		return awsV2.Config{}, errors.New("AWS Credentials expired")
-	}
-	return cfg, err
-}
-
-func GetProfiles() (profiles []string, err error) {
-	fpCred := defaults.SharedCredentialsFilename()
-	_, errCred := os.Stat(fpCred)
-	fpConf := defaults.SharedConfigFilename()
-	_, errConf := os.Stat(fpConf)
-	if os.IsNotExist(errCred) && os.IsNotExist(errConf) {
-		return nil, errConf
-	}
-	var ret []string
-	defaultReturn := &Profiles{Data: nil, Error: ""}
-	fp := defaults.SharedCredentialsFilename()
-	_, err = os.Stat(fp)
-	if os.IsNotExist(err) {
-		fp = defaults.SharedConfigFilename()
-	}
-	f, err := ini.Load(fp) // Load ini file
-	if err != nil {
-		defaultReturn.Error = err.Error()
-	} else {
-		arr := []string{}
-		for _, v := range f.Sections() {
-			if len(v.Keys()) != 0 {
-				arr = append(arr, v.Name())
-			}
-		}
-		defaultReturn.Data = arr
-	}
-	for i := 0; i < len(defaultReturn.Data); i++ {
-		spltiArr := strings.Split(defaultReturn.Data[i], " ")
-		if len(spltiArr) == 1 {
-			ret = append(ret, spltiArr[len(spltiArr)-1])
-		} else if len(spltiArr) > 1 && spltiArr[0] == "profile" {
-			ret = append(ret, spltiArr[len(spltiArr)-1])
-		}
-	}
-	return ret, nil
-}
-
-func GetLocalstackCfg(region string) (awsV2.Config, error) {
-	customResolver := awsV2.EndpointResolverFunc(func(service, region string) (awsV2.Endpoint, error) {
-		return awsV2.Endpoint{
-			URL:           "http://localhost:4566",
-			SigningRegion: region,
-		}, nil
-	})
-
-	awsLSCfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion(region),
-		config.WithEndpointResolver(customResolver),
-	)
-	if err != nil {
-		log.Fatalf("Cannot load the AWS configs: %s", err)
-	}
-	return awsLSCfg, nil
 }
